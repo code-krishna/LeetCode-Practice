be52f0a7083bb89b8a20c3f6c7945ff3a3af73a0
Vamsi Krishna Pendyala <krishna.pendiala@gmail.com>
2022-03-28 16:42:36 +0530

    Time: 0 ms (100.00%), Space: 9.7 MB (5.70%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,28 +1,51 @@
 class Solution {
 public:
+    
+    bool binSearch(vector<int> v, int target){
+        
+        int l=0, r=v.size()-1;
+        
+        while(l <= r){
+            
+            int mid = (l+r)/2;
+            if(target == v[mid]) return true;
+            
+            if(target > v[mid]) l = mid+1;
+               
+            else r = mid-1;
+            
+        }
+    
+        return false;
+    }
+    
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         
         int row=0;
         
-        for(int i=0;i<matrix.size();++i){
+        int l=0, r=matrix.size()-1;
+        
+        while(l <= r){
             
-            if(matrix[i][matrix[0].size()-1] > target){
-                 row = i;
-                 break;
+            row = (l+r)/2;
+            //cout<<row<<"\n";
+            
+            if(row!=0){
+                if(target < matrix[row][matrix[row].size()-1] && target > matrix[row-1][matrix[row].size()-1])
+                    break;
             }
             
-            else if(matrix[i][matrix[0].size()-1] == target){
-                return true;
-            }
-        }
-        
-        for(int i=0;i<matrix[row].size();++i){
+            if(target == matrix[row][matrix[row].size()-1]) return true;
+               
+            if(target < matrix[row][matrix[row].size()-1])
+                r = row-1;
             
-            if(matrix[row][i] == target)
-                return true;
+            else if(target > matrix[row][matrix[row].size()-1])
+                l = row+1;
         }
         
-        return false;
+        
+        return binSearch(matrix[row], target);
         
     }
 };
