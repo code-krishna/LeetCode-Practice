af64aaab103ab3e215209c3d075ea7badf5b84c6
Vamsi Krishna Pendyala <krishna.pendiala@gmail.com>
2022-03-30 23:59:59 +0530

    Time: 20 ms (61.35%), Space: 8.7 MB (91.63%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -2,20 +2,18 @@ class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
         
-        int m[26];
-        
-        for(int i=0;i<26;++i){
-            m[i]=0;
-        }
+        int m[26]={0,}, r[26]={0,};
         
         for(int i=0;i<magazine.size();++i){
             m[magazine[i]-'a']++;
         }
         
         for(int i=0;i<ransomNote.size();++i){
-            if(m[ransomNote[i]-'a']==0) return false;
-            
-            m[ransomNote[i]-'a']--;
+            r[ransomNote[i]-'a']++;
+        }
+        
+        for(int i=0;i<26;++i){
+            if(r[i] > m[i]) return false;
         }
         return true;
     }
