164692dfa6d54065b231582ae16abe7952cfecca
Vamsi Krishna Pendyala <krishna.pendiala@gmail.com>
2022-04-04 23:30:51 +0530

    Time: 72 ms (18.60%), Space: 32.4 MB (62.79%) - LeetHub

--- 617-merge-two-binary-trees/617-merge-two-binary-trees.cpp
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.cpp
@@ -14,44 +14,24 @@ public:
     TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
         
         if(root1==NULL && root2==NULL) return NULL;
-        
         if(root1==NULL && root2!=NULL) return root2;
-        
         if(root1!=NULL && root2==NULL) return root1;
         
-        root1->val = root1->val + root2->val;
+        int val1 = root1!=NULL ? root1->val : 0;
+        int val2 = root2!=NULL ? root2->val : 0;
         
-        if(root1->left==NULL && root2->left!=NULL){
-            TreeNode *ptr = new TreeNode(0, NULL, NULL);
-            root1->left = ptr;
-            mergeTrees(root1->left, root2->left);
-        }
+        root1->val = val1 + val2;
         
-        else if(root1->left!=NULL && root2->left==NULL){
-            TreeNode *ptr = new TreeNode(0, NULL, NULL);
-            root2->left = ptr;
-            mergeTrees(root1->left, root2->left);
-        }
+        TreeNode *ptr1 = root1!=NULL ? root1->left : NULL;
+        TreeNode *ptr2 = root2!=NULL ? root2->left : NULL;
         
-        else if(root1->left!=NULL && root2->left!=NULL){
-            mergeTrees(root1->left, root2->left);
-        }
+        root1->left = mergeTrees(ptr1, ptr2);
         
-        if(root1->right==NULL && root2->right!=NULL){
-            TreeNode *ptr = new TreeNode(0, NULL, NULL);
-            root1->right = ptr;
-            mergeTrees(root1->right, root2->right);
-        }
+        ptr1 = root1!=NULL ? root1->right : NULL;
+        ptr2 = root2!=NULL ? root2->right : NULL;
         
-        else if(root1->right!=NULL && root2->right==NULL){
-            TreeNode *ptr = new TreeNode(0, NULL, NULL);
-            root2->right = ptr;
-            mergeTrees(root1->right, root2->right);
-        }
+        root1->right = mergeTrees(ptr1, ptr2);
         
-        else if(root1->right!=NULL && root2->right!=NULL){
-            mergeTrees(root1->right, root2->right);
-        }
         
         return root1;
         
