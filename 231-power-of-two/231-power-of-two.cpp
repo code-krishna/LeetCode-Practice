d77e19c02352dd08f620ea1e390adb24eab40d27
Vamsi Krishna Pendyala <krishna.pendiala@gmail.com>
2022-04-05 16:14:45 +0530

    Time: 4 ms (39.71%), Space: 5.8 MB (69.94%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -1,11 +1,6 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        
-         for(int i=0;i<=30;++i){
-            if(n==pow(2,i)) return true;
-        }
-        
-        return false;
+        return (n>0 && (n & (n-1))==0);
     }
 };
