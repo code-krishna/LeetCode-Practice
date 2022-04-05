a3c5d7bbc26136a6a9e688679eb0da3e72d434ae
Vamsi Krishna Pendyala <krishna.pendiala@gmail.com>
2022-04-05 23:23:32 +0530

    Time: 12 ms (23.94%), Space: 16.4 MB (42.21%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.cpp
+++ 101-symmetric-tree/101-symmetric-tree.cpp
@@ -20,9 +20,9 @@ public:
     
     bool similarTree(TreeNode *root1, TreeNode *root2){
         
-        if((root1==NULL && root2!=NULL) ||(root1!=NULL && root2==NULL)) return false;
-        
         if(root1==NULL && root2==NULL) return true;
+        
+        if(root1==NULL || root2==NULL) return false;
         
         return ((root1->val==root2->val) && similarTree(root1->left, root2->right) && similarTree(root1->right, root2->left));
         
