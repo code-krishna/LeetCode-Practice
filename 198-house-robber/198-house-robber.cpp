7d13b83b7569e23d16cbdeea57ffb1dd99ded1c1
Vamsi Krishna Pendyala <krishna.pendiala@gmail.com>
2022-04-06 23:59:23 +0530

    Time: 11 ms (5.91%), Space: 7.6 MB (94.97%) - LeetHub

---
+++ 198-house-robber/198-house-robber.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        
+        if(nums.size()==1) return nums[0];
+        
+        if(nums.size()==2) return max(nums[0], nums[1]);
+        
+        nums[1] = max(nums[1],nums[0]);
+        
+        for(int i=2;i<nums.size();++i){
+            nums[i] = max(nums[i-1], nums[i] + nums[i-2]);
+        }
+        
+        return nums[nums.size()-1];
+        
+    }
+};
