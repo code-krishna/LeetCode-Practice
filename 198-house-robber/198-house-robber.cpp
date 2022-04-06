dea0ee186deb678610b031cb8ecb649d5dee1888
Vamsi Krishna Pendyala <krishna.pendiala@gmail.com>
2022-04-07 00:00:30 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (76.02%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -4,8 +4,6 @@ public:
         
         if(nums.size()==1) return nums[0];
         
-        if(nums.size()==2) return max(nums[0], nums[1]);
-        
         nums[1] = max(nums[1],nums[0]);
         
         for(int i=2;i<nums.size();++i){
