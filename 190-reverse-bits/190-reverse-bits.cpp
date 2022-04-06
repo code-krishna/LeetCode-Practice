ef4488782b92d6411dcdb6135a6f95aa380fa332
Vamsi Krishna Pendyala <krishna.pendiala@gmail.com>
2022-04-06 20:00:32 +0530

    Time: 4 ms (43.77%), Space: 6 MB (10.73%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.cpp
+++ 190-reverse-bits/190-reverse-bits.cpp
@@ -2,17 +2,13 @@ class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
 
-        uint32_t res=0, a=1, b=1<<31, bit=0;
+        uint32_t res=0, bit=0;
         
         for(int i=0;i<32;++i){
-            
-            //cout<<a<<endl;
-            bit = n&a;
+            bit = n&(1<<i);
             if(bit){
-                res |= b;
+                res |= (1<<(31-i));
             }
-            a = a << 1;
-            b = b >> 1;
         }
         
         return res;
