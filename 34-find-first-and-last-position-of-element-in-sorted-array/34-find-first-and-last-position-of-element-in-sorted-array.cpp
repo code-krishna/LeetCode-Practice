250a3eee699027f9ca99ccf56247c2e87b3cf67e
Vamsi Krishna Pendyala <krishna.pendiala@gmail.com>
2022-04-07 21:09:19 +0530

    Time: 4 ms (95.97%), Space: 13.7 MB (19.14%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -6,6 +6,9 @@ public:
         lower = lower_bound(nums.begin(), nums.end(), target);
         upper = upper_bound(nums.begin(), nums.end(), target);
         if(lower==nums.end() || *lower!=target) return {-1,-1};
-        return {(int)(lower-nums.begin()), (int)(upper-nums.begin()-1)};
+        vector<int> res;
+        res.push_back(lower-nums.begin());
+        res.push_back(upper-nums.begin()-1);
+        return res;
     }
 };
