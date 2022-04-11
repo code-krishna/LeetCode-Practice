5a54f3b487baff7dce251fe7f5e898ae8a050226
Vamsi Krishna Pendyala <krishna.pendiala@gmail.com>
2022-04-12 00:13:47 +0530

    Time: 352 ms (17.89%), Space: 21.7 MB (35.77%) - LeetHub

---
+++ 15-3sum/15-3sum.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;    
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        
+        unordered_map<int,int> m;
+        sort(nums.begin(),nums.end());
+        
+        for(int i=0;i<nums.size();++i)
+        {
+            m[nums[i]] = i;
+        }
+        
+        vector<vector<int>> result;
+        
+        for(int i=0;i<nums.size();++i)
+        { 
+            if(i>0)
+            {
+                if(nums[i] == nums[i-1])
+                    continue;
+            }
+            for(int j=i+1;j<nums.size();++j)
+            {
+                if(j>i+1)
+                {
+                    if(nums[j] == nums[j-1])
+                        continue;
+                }
+                int init_sum = nums[i]+nums[j];
+                int target = 0 - init_sum;
+                if(m.find(target)!=m.end())
+                {
+                    if(m[target]!=i && m[target]!=j && m[target] > j)
+                    {
+                        vector<int> init_result;
+                        init_result = {nums[i], nums[j], target};
+                        result.push_back(init_result);
+                    }
+                }
+            }
+        }
+        
+        return result;
+    }
+};
